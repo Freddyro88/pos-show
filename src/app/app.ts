d4a298e84d3b1bfaf88736d11ae331695f02c213
Cristian Moreno <benavidesmoreno1@gmail.com>
2026-02-06 17:42:45 +0100

    app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
   templateUrl: './app.html',
-  styleUrls: ['./app.css'],
+  styleUrl: './app.css',
 })
-export class App {}
+export class AppComponent {
+  private sw = inject(SwUpdate);
+
+  updateAvailable = false;
+
+  constructor() {
+    if (!this.sw.isEnabled) return;
+
+    this.sw.versionUpdates
+      .pipe(filter((e): e is VersionReadyEvent => e.type === 'VERSION_READY'))
+      .subscribe(() => {
+        this.updateAvailable = true; // muestra banner
+      });
+  }
+
+  reloadToUpdate() {
+    window.location.reload();
+  }
+
+  dismissUpdate() {
+    this.updateAvailable = false;
+  }
+}
