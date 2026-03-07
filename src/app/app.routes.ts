710295f21ea11d005763e2136c72aaf7a59b4906
Cristian Moreno <benavidesmoreno1@gmail.com>
2026-03-07 15:13:33 +0100

    admin dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { PosComponent } from './sales/pos.component';
 import { AdminProductsComponent } from './products/admin-products.component';
+import { AdminDashboardComponent } from './dashboard/admin-dashboard.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'pos' },
   { path: 'pos', component: PosComponent },
   { path: 'admin', component: AdminProductsComponent },
+  { path: 'dashboard', component: AdminDashboardComponent },
 ];
